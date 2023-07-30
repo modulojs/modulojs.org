8c239b5d6f47998c38d3bf8ac34554bd954f34df
michaelb <michaelpb@gmail.com>
2023-07-30 15:20:39 -0700

    Porting devlog

--- src/static/components/Page/navigation.js
+++ src/static/components/Page/navigation.js
@@ -16,16 +16,32 @@
     ],
     articles: [
         {
-            title: "Example article",
-            byline: "Example author",
-            estimate: "5 minute read",
-            file: "articles/example-article.html",
+            file: "devlog/2023-05.html",
+            title: "Stable builds with the 0.0.50 release",
         },
         {
-            title: "Another article",
-            byline: "Another example",
-            estimate: "10 minute read",
-            file: "articles/another-article.html",
+            file: "devlog/2022-12.html",
+            title: "Intro slides",
+        },
+        {
+            file: "devlog/2022-09.html",
+            title: "Alpha release",
+        },
+        {
+            file: "devlog/2022-07.html",
+            title: "HTML-first",
+        },
+        {
+            file: "devlog/2022-03.html",
+            title: "Prealpha",
+        },
+        {
+            file: "devlog/2021-09.html",
+            title: "Fish",
+        },
+        {
+            file: "devlog/2021-01.html",
+            title: "FAQ",
         },
     ],
 
