ab83c999a1677e7401ffc6b87bcca68b6da5eb93
michaelb <michaelpb@gmail.com>
2023-08-03 07:23:08 -0700

    Reorging into editor and layout libraries

--- src/static/data/links/examples.js
+++ src/static/data/links/examples.js
@@ -1,7 +1,28 @@
 {
-    demoGrid: [
+    components: [
         {
-            file: "/static/demos/",
+            file: "/static/demos/eg/Hello.html",
+        },
+        {
+            file: "/static/demos/eg/Simple.html",
+        },
+        {
+            file: "/static/demos/eg/ToDo.html",
+        },
+        {
+            file: "/static/demos/eg/JSON.html",
+        },
+        {
+            file: "/static/demos/eg/Scatter.html",
+        },
+        {
+            file: "/static/demos/eg/Search.html",
+        },
+        {
+            file: "/static/demos/eg/Components.html",
+        },
+        {
+            file: "/static/demos/eg/MemoryGame.html",
         },
     ],
 }
