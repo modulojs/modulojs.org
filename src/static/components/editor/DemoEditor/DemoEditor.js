aae5b7ee490e4961a03c27226d800db9f9bd569f
michaelb <michaelpb@gmail.com>
2024-01-25 15:38:45 -0800

    Cleaning up duplicate files

--- src/static/components/editor/DemoEditor/DemoEditor.js
+++ src/static/components/editor/DemoEditor/DemoEditor.js
@@ -13,77 +13,103 @@ const DEFAULT_EXPORT_TEMPLATE = (`
 {{ usage|safe }}
 `).trim();
 
-function editorMount({ el }) {
+function editorMount({ el, value }) {
+    if (state.buffers.length === 0) {
+        _updateState(); // getting called from hydrating mount
+    }
     element.editor = el;
+    el._onEditorValueChange = bufferValue => {
+        state.buffers[state.selectedBuffer].value = bufferValue;
+    };
+    el.rerender(); // Force editor immediate rerender, so the stateChangedCallback gets attached
+    el.stateChangedCallback(null, state.buffers[state.selectedBuffer].value, null); // Force update
 }
 
-function exampleMount({ el }) {
-    element.exampleEditor = el;
+function editorUnmount({ el, value }) {
+    delete el._onEditorValueChange; // tidy up references
+    delete element.editor;
+}
+
+function newBuffersFromElement({ value, src, example, exptemplate }, elem) {
+    let isFetching = false;
+    const buffers = [
+        {
+            name: 'default',
+            value: value,
+        },
+        {
+            title: 'Usage',
+            name: 'usage',
+            subpaneClass: true,
+            bannerClass: true,
+            value: example || `<x-${ state.componentName }></x-${ state.componentName }>`,
+        },
+        {
+            title: 'Export Template (Read Only)',
+            name: 'export',
+            subpaneClass: true,
+            bannerClass: true,
+            //isHidden: true, // (implemented)
+            isReadOnly: true,
+            value: DEFAULT_EXPORT_TEMPLATE,
+        },
+    ];
+    if (elem.hasAttribute('modulo-value') && !value) {
+        buffers[0].value = elem.getAttribute('modulo-value');
+    }
+    if (src && !buffers[0].value) {
+        buffers[0].isFetching = true;
+        window.fetch(src)
+            .then(response => response.text())
+            .then(text => {
+                delete buffers[0].isFetching; // Clear fetching status
+                buffers[0].value = text; // update the text with fetch results
+                elem.setAttribute('modulo-value', text); // store in DOM for built page
+                elem.rerender(); // Force container rerender
+            });
+    }
+    return buffers;
+}
+
+function setBuffer(index) {
+    state.selectedBuffer =  index;
+}
+
+function getDemoValue(name) {
+    if (name === 'component') {
+        return getComponentName(); // special one!
+    }
+    modulo.assert(state.buffers[0].name === 'default', '...');
+    const i = ['value', 'example', 'export' ].indexOf(name);
+    modulo.assert(i > -1, 'Not expected: ' + name);
+    const buffer = state.buffers[i];
+    return buffer.value || '';
 }
 
 function _updateState() {
-    if (element.editor) {
-        state.value = element.editor.value;
-    }
-    if (props.value && !state.value) {
-        state.value = props.value;
-    }
-    if (element.hasAttribute('modulo-value') && !state.value) {
-        state.value = element.getAttribute('modulo-value');
-    }
-    if (state.showAltEditor === null) {
-        state.showAltEditor = !!props.editex; // Default to if it's an "example editor"
-    }
     state.showEditor = !props.collapsed;
     state.componentName = getComponentName();
-
-    if (!state.exampleCode) { // Init with props if available
-        state.exampleCode = props.example || `<x-${ state.componentName }></x-${ state.componentName }>`;
+    if (state.buffers.length === 0) { // No buffers! Populate from props and element
+        state.buffers = newBuffersFromElement(props, element);
+        state.selectedBuffer = props.editex ? 1 : 0; // First time, set to 0 if not an example editor
     }
-    if (!state.exportCode) { // Init with props if available
-        state.exportCode = props.exptemplate || DEFAULT_EXPORT_TEMPLATE;
-    }
-
-    if (state.showAltEditor) { // Handle updating alt-editor stuff
-        //state.altEditorList = [ 'Usage', 'Includes', 'Export' ];
-        //state.altEditorList = [ 'Usage', 'Export' ];
-        state.altEditorList = null; // disabled for now
-        if (state.altEditorSelected === 'Export') {
-            state.altEditorCode = state.exportCode;
-        } else if (state.altEditorSelected === 'Includes') {
-            state.altEditorCode = state.includesCode;
-        } else {
-            state.altEditorCode = state.exampleCode;
-        }
-    }
-
-    if (element.exampleEditor) { // Use user-supplied example code
-        if (state.altEditorSelected === 'Export') {
-            // Update the export template
-            state.exportCode = element.exampleEditor.value;
-        } else if (state.altEditorSelected === 'Includes') {
-            // Update with includes code
-            state.includesCode = element.exampleEditor.value;
-        } else {
-            // In all other cases, just use exampleCode
-            state.exampleCode = element.exampleEditor.value;
-        }
-    }
+    state.demoBox = { // "Transform" the buffers into the expected demo arrangement
+        value: getDemoValue('value'),
+        component: getDemoValue('component'),
+        example: getDemoValue('example'),
+    };
 }
 
 function prepareCallback() {
     _updateState();
-    const e = element; // TODO: Refactor
-    if (props.src && !state.value && state.isFetching === null) {
-        state.isFetching = true;
-        window.fetch(props.src)
-            .then(response => response.text())
-            .then(text => {
-                e.cparts.state.data.value = text;
-                e.cparts.state.data.isFetching = false;
-                e.setAttribute('modulo-value', text);
-                e.cparts.state.propagate('value', text);
-            });
+    const buffer = state.buffers[state.selectedBuffer];
+    return { buffer };
+}
+
+function renderCallback() {
+    if (element.hasAttribute('modulo-mount-html')) {
+        element.removeAttribute('modulo-mount-html');
+        component.innerHTML = null; // Lock first render when re-hydrating
     }
 }
 
@@ -111,16 +137,28 @@ function run() {
 
 function save() {
     // Saves as HTML
+    // TODO: Add different save template options:
+    //    -  Editor: Includes <Modulo -src="https://modulojs.org/static/components/editor/"></Modulo>
+    //        then embeds editor in exact current state as this one
+    //    -  Embed: Saves a single HTML file with all scripts inlined (can be with above, as well)
+    //    -  Embed with Editor: (Combo of above)
     _updateState();
-    const fullText = toEmbed(state.value, state.componentName, state.exampleCode, state.includesCode, state.exportCode);
-    saveFileAs(`Modulo_${ state.componentName }.html`, fullText);
+    const { component, value, example } = state.demoBox;
+    const includesCode = '';
+    const exportCode = getDemoValue('export');
+    const fullText = toEmbed(value, component, example, includesCode, exportCode);
+    saveFileAs(`Modulo_${ component }.html`, fullText);
 }
+
 
 function open() {
     // Opens in ACE editor
     _updateState();
-    const fullText = toEmbed(state.value, state.componentName, state.exampleCode, state.includesCode, state.exportCode);
-    const path =`/demo/${ state.componentName }.html`
+    const { component, value, example } = state.demoBox;
+    const includesCode = '';
+    const exportCode = getDemoValue('export');
+    const fullText = toEmbed(value, component, example, includesCode, exportCode);
+    const path =`/demo/${ component }.html`
     localStorage.setItem(LOCAL_STORAGE_PREFIX + path, fullText);
     window.location.href = (EDITOR_LOCATION + '?ls=' + path);
 }
